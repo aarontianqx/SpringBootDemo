80c0ff50406257a413b023a863e02b681b404bff
Tian, Aaron - Contractor <aarontian@contractor.ea.com>
2020-09-22 18:00:51 +0800

    reading list for chapter-02

---
+++ src/main/java/net/aarontian/springboot/demo/readinglist/ReadingListRepository.java
@@ -0,0 +1,10 @@
+package net.aarontian.springboot.demo.readinglist;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReadingListRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByReader(String reader);
+
+}
---
+++ src/test/java/net/aarontian/springboot/demo/SpringBootDemoTests.java
@@ -0,0 +1,13 @@
+package net.aarontian.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
